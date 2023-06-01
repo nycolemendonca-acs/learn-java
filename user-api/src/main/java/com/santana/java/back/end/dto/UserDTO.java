fd8ae067ba35a4789224a890b31de31d2e322924
nycolemendonca-acs <135253384+nycolemendonca-acs@users.noreply.github.com>
2023-06-01 17:31:44 -0300

    Add files via upload

---
+++ user-api/src/main/java/com/santana/java/back/end/dto/UserDTO.java
@@ -0,0 +1,28 @@
+package com.santana.java.back.end.dto;
+
+import com.santana.java.back.end.model.User;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class UserDTO {
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+    @NotBlank(message = "CPF é obrigatório")
+    private String cpf;
+    private String endereco;
+    @NotBlank(message = "E-mail é obrigatório")
+    private String email;
+    private String telefone;
+    private LocalDateTime dataCadastro;
+
+}
