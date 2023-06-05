14e8c0d9b5a78e7e92c6eb2a21fc4cc8bae03dec
nycolemendonca-acs <135253384+nycolemendonca-acs@users.noreply.github.com>
2023-06-05 10:47:10 -0300

    Add files via upload

---
+++ desbravando-java-e-orientacao-a-objetos/capitulo01/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        args[0] = "Java";
+        args[1] = "Nycole";
+
+        System.out.println("O primeiro de muitos códigos escritos em "
+                            + args[0] + " pela " + args[1] + " !");
+    }
+}
